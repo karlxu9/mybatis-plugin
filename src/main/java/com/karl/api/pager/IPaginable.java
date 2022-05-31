b589f6fc4132a5623f155a33ec2189d925a139a6
徐煜邦 <yubang.xu>
2022-05-31 15:59:53 +0800

    分页插件

---
+++ src/main/java/com/karl/api/pager/IPaginable.java
@@ -0,0 +1,99 @@
+/**
+ *
+ */
+package com.karl.api.pager;
+
+/**
+ * 模块描述: 【分页接口】
+ *
+ * @Author: Mr. YuBang.Xu
+ * @Date: 2022/5/31$ 11:29$
+ * @since: 1.8.0
+ * @version: 1.0.0
+ */
+public interface IPaginable<T> {
+
+    /**
+     * 总记录数
+     *
+     * @return
+     */
+    int getTotalCount();
+
+    /**
+     * 总页数
+     *
+     * @return
+     */
+    int getTotalPage();
+
+    /**
+     * 每页记录数
+     *
+     * @return
+     */
+    int getPageSize();
+
+    /**
+     * 当前页号
+     *
+     * @return
+     */
+    int getPageNo();
+
+    /**
+     * 是否第一页
+     *
+     * @return
+     */
+    boolean isFirstPage();
+
+    /**
+     * 是否最后一页
+     *
+     * @return
+     */
+    boolean isLastPage();
+
+    /**
+     * 返回下页页码
+     *
+     * @return
+     */
+    int getNextPage();
+
+    /**
+     * 返回上页页码
+     *
+     * @return
+     */
+    int getPrePage();
+
+    /**
+     * 取得当前页显示的项的起始序号
+     *
+     * @return
+     */
+    int getBeginIndex();
+
+    /**
+     * 取得当前页显示的末项序号
+     *
+     * @return
+     */
+    int getEndIndex();
+
+    /**
+     * 获取开始页
+     *
+     * @return
+     */
+    int getBeginPage();
+
+    /**
+     * 获取结束页
+     *
+     * @return
+     */
+    int getEndPage();
+}
---
+++ src/main/java/com/karl/api/pager/PageInterceptor.java
@@ -0,0 +1,150 @@
+package com.karl.api.pager;
+
+
+import com.karl.util.ReflectionUtils;
+import org.apache.ibatis.executor.parameter.ParameterHandler;
+import org.apache.ibatis.executor.statement.RoutingStatementHandler;
+import org.apache.ibatis.executor.statement.StatementHandler;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.mapping.ParameterMapping;
+import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.scripting.defaults.DefaultParameterHandler;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * 模块描述: 【分页插件拦截器】
+ *
+ * @Author: Mr. YuBang.Xu
+ * @Date: 2022/5/31$ 11:37$
+ * @since: 1.8.0
+ * @version: 1.0.0
+ */
+@Intercepts(@Signature(method = "prepare", type = StatementHandler.class, args = {Connection.class, Integer.class}))
+public class PageInterceptor implements Interceptor {
+    private String databaseType;
+
+    @Override
+    public Object intercept(Invocation invocation) throws Throwable {
+        RoutingStatementHandler handler = (RoutingStatementHandler) invocation.getTarget();
+        StatementHandler delegate = (StatementHandler) ReflectionUtils.getFieldValue(handler, "delegate");
+        BoundSql boundSql = delegate.getBoundSql();
+        Object parameterObject = boundSql.getParameterObject();
+        if (parameterObject instanceof Page<?>) {
+            Page<?> page = (Page<?>) parameterObject;
+            MappedStatement mappedStatement = (MappedStatement) ReflectionUtils.getFieldValue(delegate, "mappedStatement");
+            Connection connection = (Connection) invocation.getArgs()[0];
+            String sql = boundSql.getSql();
+            if (page.isFull()) {
+                this.setTotalCount(page, mappedStatement, connection);
+            }
+            page.setTimestamp(System.currentTimeMillis());
+            String pageSql = this.getPageSql(page, sql);
+            ReflectionUtils.setFieldValue(boundSql, "sql", pageSql);
+        }
+        return invocation.proceed();
+    }
+
+    /**
+     * 获取分页SQL
+     *
+     * @param page
+     * @param sql
+     * @return
+     */
+    private String getPageSql(Page<?> page, String sql) {
+        StringBuffer sqlBuffer = new StringBuffer(sql);
+        if ("mysql".equalsIgnoreCase(databaseType)) {
+            return getMysqlPageSql(page, sqlBuffer);
+        } else if ("oracle".equalsIgnoreCase(databaseType)) {
+            return getOraclePageSql(page, sqlBuffer);
+        } else if ("hsqldb".equalsIgnoreCase(databaseType)) {
+            return getHSQLDBPageSql(page, sqlBuffer);
+        }
+        return sqlBuffer.toString();
+    }
+
+    private String getHSQLDBPageSql(Page<?> page, StringBuffer sqlBuffer) {
+        int offset = (page.getPageNo() - 1) * page.getPageSize() + 1;
+        return "select limit " + offset + " " + page.getPageSize() + " * from (" + sqlBuffer.toString() + ")";
+    }
+
+    private String getOraclePageSql(Page<?> page, StringBuffer sqlBuffer) {
+        int offset = (page.getPageNo() - 1) * page.getPageSize() + 1;
+        sqlBuffer.insert(0, "select u.*, rownum r from (").append(") u where rownum < ")
+                .append(offset + page.getPageSize());
+        sqlBuffer.insert(0, "select * from (").append(") where r > = ").append(offset);
+        return sqlBuffer.toString();
+    }
+
+    private String getMysqlPageSql(Page<?> page, StringBuffer sqlBuffer) {
+        int offset = (page.getPageNo() - 1) * page.getPageSize();
+        sqlBuffer.append(" limit ").append(offset).append(",").append(page.getPageSize());
+        return sqlBuffer.toString();
+    }
+
+    /**
+     * 获取总的条数
+     *
+     * @param page
+     * @param mappedStatement
+     * @param connection
+     */
+    private void setTotalCount(Page<?> page, MappedStatement mappedStatement, Connection connection) {
+        BoundSql boundSql = mappedStatement.getBoundSql(page);
+        String sql = boundSql.getSql();
+        String countSql = this.getCountSql(sql);
+        List<ParameterMapping> parameterMappings = boundSql.getParameterMappings();
+        BoundSql countBoundSql = new BoundSql(mappedStatement.getConfiguration(), countSql, parameterMappings, page);
+        ParameterHandler parameterHandler = new DefaultParameterHandler(mappedStatement, page, countBoundSql);
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            pstmt = connection.prepareStatement(countSql);
+            parameterHandler.setParameters(pstmt);
+            rs = pstmt.executeQuery();
+            if (rs.next()) {
+                int totalCount = rs.getInt(1);
+                page.setTotalCount(totalCount);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            if (pstmt != null) {
+                try {
+                    pstmt.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
+    }
+
+    private String getCountSql(String sql) {
+        return "select count(1) " + sql.substring(sql.toLowerCase().indexOf("from"));
+    }
+
+    @Override
+    public Object plugin(Object o) {
+        return Plugin.wrap(o, this);
+    }
+
+    @Override
+    public void setProperties(Properties properties) {
+        this.databaseType = properties.getProperty("databaseType");
+    }
+}
